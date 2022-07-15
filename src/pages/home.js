34fd29d3291064df0a0761fedaf0d2e7cd62e394
Akash Pal <creativeakashpal@gmail.com>
2022-07-15 21:13:04 +0530

    minor changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,22 @@
 function home() {
     const content = document.querySelector('#content');
+    content.classList.add('home');
+
+    const siteInfo = document.createElement('div');
+    siteInfo.classList.add('site-info')
 
     const h1 = document.createElement('h1');
     h1.textContent = "World's Best Kitchen To Satisfy Your Hunger";
 
     const h2 = document.createElement('h2');
-    h2.textContent = "Tasty food that will not empty your wallet";
+    h2.textContent = "Tasty food that will not empty your pocket";
 
     const order = document.createElement('button');
     order.textContent = 'Order Now';
 
-    content.append(h1, h2, order);
+    siteInfo.append(h1, h2, order);
+
+    content.append(siteInfo);
 }
 
 export default home;
