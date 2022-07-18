cf9f5d1a8b92ebcf73191e7fe1668aa52f8db082
Akash Pal <creativeakashpal@gmail.com>
2022-07-18 12:17:09 +0530

    fixed repeating contact page & added nav highlight

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,18 @@
 import locationIcon from '../asset/icons/location.svg';
 import phoneIcon from '../asset/icons/phone.svg';
 import emailIcon from '../asset/icons/email.svg';
+import menu from './menu';
 
 const content = document.getElementById('content');
 const contactDiv =  document.createElement('div');
 contactDiv.classList.add('contact-div');
 
+const contactUsForm = document.createElement('div');
+const contactInfoDiv = document.createElement('div');
+
 function contact() {
     
     content.classList.add('contact');
-    content.classList.remove('home');
-    content.classList.remove('menu');
 
     const contactUs = document.createElement('div');
     const h1 =  document.createElement('h1');
@@ -19,18 +21,21 @@ function contact() {
 
     contactUs.append(h1);
     content.append(contactUs);
-    content.append(contactDiv)
+    content.append(contactDiv);
+
+    highligthNav();
 
     createContactInfo();
     createForm();
+
 }
 
 function createContactInfo() {
-    const contactInfoDiv = document.createElement('div');
+    
     contactInfoDiv.innerHTML = 
         `<div class="contact-info">
             <img src="${phoneIcon}" alt="">
-            <p>+91-9223456780</p>
+            <p>+91-1234567890</p>
         </div>
         <div class="contact-info">
             <img src="${locationIcon}" alt="">
@@ -46,21 +51,36 @@ function createContactInfo() {
 
 
 function createForm() {
-    const contactUsForm = document.createElement('div');
+    
     contactUsForm.innerHTML = 
         `<form action="">
             <div class="name">
-                <input type="text" id="name" name="name" placeholder="Your Name">
+                <input type="text" id="name" name="name" placeholder="Your Name" required>
             </div>
             <div class="email">
-                <input type="email" id="email" name="email" placeholder="Email">
+                <input type="email" id="email" name="email" placeholder="Email" required>
             </div>
             <div class="message">
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Leave us a message"></textarea>
+                <textarea name="message" id="message" cols="30" rows="8" placeholder="Leave us a message"></textarea required>
             </div>
-            <button type="submit">Send</button>
+            <button type="submit" class="submit">Send</button>
         </form>`
 
     contactDiv.append(contactUsForm);
 }
+
+function highligthNav() {
+    const homeNav = document.getElementById('home');
+    const contactNav = document.getElementById('contact');
+    const menuNav = document.getElementById('menu');
+
+    contactNav.style.backgroundColor = '#1B263C';
+    contactNav.style.color = '#fff';
+
+    homeNav.style.backgroundColor = '#fff';
+    homeNav.style.color = '#1B263C'
+    menuNav.style.backgroundColor = '#fff';
+    menuNav.style.color = '#1B263C'
+}
+
 export default contact;
