f8ab579a910df686f0da38792f9947e2bf6a47a8
Akash Pal <creativeakashpal@gmail.com>
2022-07-18 12:16:13 +0530

    made order button functional & added nav highlight

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,6 @@
+import menu from "./menu";
+import clear from "../functions/clear";
+
 function home() {
     const content = document.querySelector('#content');
     content.classList.add('home');
@@ -13,10 +16,31 @@ function home() {
 
     const order = document.createElement('button');
     order.textContent = 'Order Now';
+    order.classList.add('order');
+
+    order.addEventListener('click' , function() {
+        clear();
+        menu();
+    });
 
     siteInfo.append(h1, h2, order);
+    highligthNav();
 
     content.append(siteInfo);
+}
+
+function highligthNav() {
+    const homeNav = document.getElementById('home');
+    const contactNav = document.getElementById('contact');
+    const menuNav = document.getElementById('menu');
+
+    homeNav.style.backgroundColor = '#1B263C';
+    homeNav.style.color = '#fff';
+
+    contactNav.style.backgroundColor = '#fff';
+    contactNav.style.color = '#1B263C';
+    menuNav.style.backgroundColor = '#fff';
+    menuNav.style.color = '#1B263C';
 }
 
 export default home;
