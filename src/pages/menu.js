7356cc1f0ae2a37faa0edf6bba3f546e5a0f9a1c
Akash Pal <creativeakashpal@gmail.com>
2022-07-18 20:37:02 +0530

    moved images to img tag from background image

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,16 +1,23 @@
+import fishCurry from '../asset/images/fish-curry.jpg';
+import samosa from '../asset/images/samosa.jpg';
+import idli from '../asset/images/idli.jpg';
+import chickenBiryani from '../asset/images/chicken-biryani.jpg';
+import pavBhaji from '../asset/images/pav-bhaji.jpg';
+import pakoda from '../asset/images/pakoda.jpg';
+
 const content = document.getElementById('content');
 
 function menu() {
     content.classList.add('menu');
-    createMenuDiv('Samosa', ' ₹49', 'samosa', 'Perfect mix of spicy & crunchy. Enjoy your evening with samosa and chai');
-    createMenuDiv('Fish Curry', ' ₹399', 'fish-curry', 'Spicy & healthy , a high protein food. Helthy food that tastes good');
-    createMenuDiv('Idli', ' ₹149', 'idli', 'Healthy & basic made from rice, enjoy with coconut chutney');
-    createMenuDiv('Chicken Biryani', ' ₹489', 'chicken-biryani', 'Craving chicken? Best dish to satisfy your chicken craving & hunger');
-    createMenuDiv('Pav Bhaji', ' ₹199', 'pav-bhaji', 'Need something spicy you came at right place. Your perfect cheat meal');
-    createMenuDiv('Pakode', ' ₹99', 'pakoda', 'Rainy outside? Pefect to have pakoda & chai on a rainy day');
+    createMenuDiv('Samosa', ' ₹49', samosa, 'Perfect mix of spicy & crunchy. Enjoy your evening with samosa and chai');
+    createMenuDiv('Fish Curry', ' ₹399', fishCurry, 'Spicy & healthy , a high protein food. Helthy food that tastes good');
+    createMenuDiv('Idli', ' ₹149', idli, 'Healthy & basic made from rice, enjoy with coconut chutney');
+    createMenuDiv('Chicken Biryani', ' ₹489', chickenBiryani, 'Craving chicken? Best dish to satisfy your chicken craving & hunger');
+    createMenuDiv('Pav Bhaji', ' ₹199', pavBhaji, 'Need something spicy you came at right place. Your perfect cheat meal');
+    createMenuDiv('Pakode', ' ₹99', pakoda, 'Rainy outside? Pefect to have pakoda & chai on a rainy day');
 }
 
-function createMenuDiv(name, cost, imageSrc, desc) {
+function createMenuDiv(name, cost, img, desc) {
     const menuDiv = document.createElement('div');
     menuDiv.setAttribute('id', 'menu-div');
 
@@ -19,8 +26,13 @@ function createMenuDiv(name, cost, imageSrc, desc) {
     const itemDesc = document.createElement('p');
 
     const itemImage = document.createElement('div');
-    itemImage.classList.add('image');
-    itemImage.classList.add(imageSrc);
+    itemImage.classList.add('image-div');
+
+    const itemImg = document.createElement('img');
+    itemImg.classList.add('image');
+    itemImg.src = img;
+
+    itemImage.append(itemImg);
 
     const itemDiv = document.createElement('div');
     itemCost.textContent = cost;
